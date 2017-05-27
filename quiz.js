78d61ba87d038f6768c39d4ba087b59e84639d8e
Surbhi83 <surabhi.gupta.sai@gmail.com>
2017-05-27 17:12:47 -0400

    Added code

--- quiz.js
+++ quiz.js
@@ -2,69 +2,71 @@ var allQuestions = [
     {
         question: "Q 1 - Which of the following type of variable is visible everywhere in your JavaScript code?",
         choices: ["global variable", "local variable", "Both of the above","None of the above"],
-        correctAnswer:"global variable"
+        correctAnswer: 0
     },
   
     {
         question: "Q 2 - Which built-in method reverses the order of the elements of an array?",
         choices: ["changeOrder(order)", "reverse()", "sort(order)", "None of the above"],
-        correctAnswer: "reverse()"
+        correctAnswer: 1
     },
 
     {
         question: "Q 3 - Which of the following jQuery selector select elements whose css class is some-class?",
         choices: ["$('some-class')", "$('#some-class')", "$('.some-class')", "None of the above"],
-        correctAnswer: "$('.some-class')"
+        correctAnswer: 2
     },
    
     {
         question: "Q 4 - Which of the following jQuery method apply a style on an element?",
         choices: ["addStyle(classes)", "addClass( classes )", "addCSSClass(classes)", "None of the above"],
-        correctAnswer: "addClass( classes )"
+        correctAnswer: 1
     },
   
 
     {
         question: "Q 5 - Which of the following jQuery method selects a subset of the matched elements?",
         choices: ["subset( selector )", "getSubset( selector )", "slice(selector)", "None of the above"],
-        correctAnswer: "slice(selector)"
+        correctAnswer: 2
     },
   
     {
         question: "Q 6 - Which of the following jQuery method gets a set of elements containing all of the unique immediate children of each of the matched set of elements?",
         choices: ["getChild(selector)","children([selector])","getChildren(selector)","None of the above"],
-        correctAnswer: "children([selector])"
+        correctAnswer: 1
     },
 
 
     {
         question: "Q 7 - Which of the following jQuery method gets the inner height (excluding the border) of an element?",
         choices: ["getCSSHeight()","innerHeight()","getInnerHeight()","None of the above"],
-        correctAnswer: "innerHeight()"
+        correctAnswer: 1
     },
     
 
      {
         question: "Q 8 - Which of the following jQuery method removes set of matched elements?",
         choices: ["empty()","delete()","remove(expr)","None of the above"],
-        correctAnswer: "remove(expr)"
+        correctAnswer: 2
     },
 
      {
         question: "Q 9 - Which of the following jQuery method checks if event.stopPropagation() was ever called on this event object?",
         choices: ["isDefaultPrevented()","isPropagationStopped()","isImmediatePropagationStopped()","None of the above"],
-        correctAnswer: "isPropagationStopped()"
+        correctAnswer: 1
     },
 
      {
         question: "Q 10 - Which of the following jQuery method can be used to attach a function to be executed whenever AJAX request completed successfully?",
         choices: ["ajaxStart(callback)","ajaxSuccess(callback)","ajaxSend(callback)","ajaxStop(callback)"],
-        correctAnswer: "ajaxSuccess(callback)"
+        correctAnswer: 1
     }
 ];
+
 var submitBtn = $('#myBtn');
 var currentQuestion = 0;
 var tally = 0;
+
 var quizForm = $('#quiz');
 var question;
 var choices;
@@ -84,12 +86,14 @@ function firstFunc() {
 function askQuestion () {
     if(currentQuestion < totalCount)
     {
-    $('#counter').html("<div>  Question "+( currentQuestion+1)+" out of "+totalCount+"</div>");
+    $('#counter').html("<div> Question "+(currentQuestion+1)+" out of "+totalCount+"</div>");
     }
     else
     {
       $('#counter').html("");  
     }
+
+
     choices = allQuestions[currentQuestion].choices;
     question = allQuestions[currentQuestion].question;
     if (currentQuestion < allQuestions.length) {
@@ -102,11 +106,13 @@ function askQuestion () {
         
         }
         quizForm.html(answerhtml);
+
+
     }
 }
 
 function lookForChecked() {
-    if (submitBtn.val()==="Restart Quiz")
+    if (submitBtn.val()==="RestartQuiz")
     {
 
      location.reload();
@@ -117,14 +123,22 @@ function lookForChecked() {
          console.log('you have ended the quiz');
          calcQuiz();
          $('#counter').html("");
-         $('#answerMessage').html("");  
+         $('#answerMessage').html(""); 
+       
     }
+
     if (radioButtons.length > 1 &&  submitBtn.val()==="Check my answer") {
+
+        
             var checked = false;
         for (var i = 0; i < radioButtons.length; i++) {
             var selection = radioButtons[i];
+
              if (selection.checked) {
+             
                 var index = [i];
+            
+
                 if (i === allQuestions[currentQuestion].correctAnswer) {
                     tally++;
                 $('#answerMessage').html(
@@ -132,7 +146,7 @@ function lookForChecked() {
                 
                 }
                 else{
-                $('#answerMessage').html("<div class='red'> <img class='sadImage' src='http://clipart-library.com/data_images/71350.png'> Your answer is incorrect. Correct answer is "+ allQuestions[currentQuestion].correctAnswer +"</div>");
+                $('#answerMessage').html("<div class='red'> <img class='sadImage' src='http://clipart-library.com/data_images/71350.png'> Your answer is incorrect.</div>");
                 
 
                 }
@@ -151,11 +165,16 @@ function lookForChecked() {
         $('#answerMessage').html("");
         askQuestion();
     } 
+ 
+
+
+   
+        
 }
 
 function calcQuiz() {
     quizForm.html("<h1>You have finished the quiz</h1><p class='total'>You scored a total of " + tally + " out of " + allQuestions.length + "</h1>");
-    submitBtn.val("Restart Quiz");
+    submitBtn.val("RestartQuiz");
     // submitBtn.remove();
     // restartQuiz.html("<input type='submit' class='restartBtn' id='restartQ' value='RestartQuiz' >");
 }
